bf9793432ea0f08775064fb9405a9dfa48b363a5
yhupaul <yhupaul@hotmail.com>
2021-07-29 16:05:15 -0700

    after mocha test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,27 @@
 const request = require('request');
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const fetchBreedDescription = function(breedName, callback) {
+
+const fetchBreedDescription = function (breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
   request(url, (error, response, body) => {
     const data = JSON.parse(body);
-    if (!data[0]) {
-      console.log(`The requseted ${breedName} is not found`);
-    } else {
-      callback(null, data[0].description);
-      // console.log(data[0].description);
-    }
+
     if (error) {
       callback(error, null);
-    //console.log("error");
+      return;
+      //console.log("error");
     }
-  });
+
+    if (!data[0]) {
+      callback(`The requseted ${breedName} is not found`, null);
+      return;
+    }
+
+    callback(null, data[0].description);
+    //console.log(data[0].description);
+
+  })
 };
 
 
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,33 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+
+  it('returns invalid/non-existent breed', (done) => {
+    fetchBreedDescription('rohit', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(desc, null);
+
+      const expectedDesc = "The requseted rohit is not found"
+      // compare returned description
+      assert.equal(expectedDesc, err);
+
+      done();
+    });
+  });
+
+});
