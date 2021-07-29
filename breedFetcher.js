4ef2c3a8e7c21ae59464030c4e83849098eda376
yhupaul <yhupaul@hotmail.com>
2021-07-29 15:16:36 -0700

    callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,23 @@
 const request = require('request');
-const breed = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const breedName = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (!data[0]) {
-    console.log(`The requseted ${breed} is not found`);
-  } else {
-    console.log(data[0].description);
-  }
-  if (error) {
-    console.log("error");
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      console.log(`The requseted ${breedName} is not found`);
+    } else {
+      callback(null, data[0].description);
+      // console.log(data[0].description);
+    }
+    if (error) {
+      callback(error, null);
+    //console.log("error");
+    }
+  });
+};
 
-});
+
+
+module.exports = { fetchBreedDescription };
