965f56d5a43359b14cc620bc8500f15d3de10f0b
yhupaul <yhupaul@hotmail.com>
2022-02-17 21:56:13 -0800

    review

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,22 @@
 const request = require('request');
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, resp, body) => {
-  // console.log(body);
-  // console.log(typeof body);
+  request(url, (error, resp, body) => {
+    if (error) {
+      return callback(error, null);
+    }
 
-  if (error) {
-    return console.log('Failed to request details: ', error);
-  }
+    const data = JSON.parse(body);
+    const breed = data[0];
 
-  const data = JSON.parse(body);
-  // console.log(data);
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback('Breed Not Found');
+    }
+  });
+};
 
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${breedName}`);
-  }
-});
+module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -6,6 +6,6 @@ fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log('Error fetch details:', error);
   } else {
-    console.log(desc);
+    console.log('Description: ', desc);
   }
 });
